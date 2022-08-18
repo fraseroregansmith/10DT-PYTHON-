5ca81a029fca49f034ed6a53e8ff1ea8632754cb
fraseroregansmith <103541186+fraseroregansmith@users.noreply.github.com>
2022-08-18 12:14:02 +1200

    Add files via upload

---
+++ Lesson1/basic_var_and_output_with_labels.py
@@ -0,0 +1,9 @@
+name = "Is Walter Whites"
+height = "179cm"
+adress = "308 Negra Arroyo Lane"
+telephoneNumber = "(505) 193-0809"
+
+print( "My name", name)
+print("I am ", height)
+print("I live at",adress)
+print("Phone number",telephoneNumber)
