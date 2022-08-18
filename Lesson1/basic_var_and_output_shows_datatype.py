c707953de73cb70bfffa6870ac94d5c49699d730
fraseroregansmith <103541186+fraseroregansmith@users.noreply.github.com>
2022-08-18 12:19:42 +1200

    Add files via upload

---
+++ Lesson1/basic_var_and_output_shows_datatype.py
@@ -0,0 +1,16 @@
+# First Name Last Name
+# Variables
+
+# Declaration of Variables 
+name = "Elephant"
+numLegs = 4
+height = "4.5 Metres"
+hasTail = True 
+
+# Output to screen
+ 
+
+print(name)
+print(numLegs)
+print(height)
+print(hasTail)
