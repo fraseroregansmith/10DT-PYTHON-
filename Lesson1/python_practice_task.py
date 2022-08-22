98949fbef9cc46285f2b53ab8a77e338c9bd0c5c
fraseroregansmith <103541186+fraseroregansmith@users.noreply.github.com>
2022-08-23 09:18:18 +1200

    Add files via upload

---
+++ Lesson1/python_practice_task.py
@@ -0,0 +1,17 @@
+# Fraser O'Regan Smith
+# Practice Task
+
+#Variable Declaration
+
+
+name = "Walter White"
+height = "179cm"
+adress = "308 Negra Arroyo Lane"
+telephoneNumber = "(505) 193-0809"
+HasAllergy = False
+
+print( "My name is", name)
+print("I am ", height)
+print("I live at",adress)
+print("Phone number",telephoneNumber)
+print("To say that I have an allergy, that would be a", HasAllergy, "statement.")
