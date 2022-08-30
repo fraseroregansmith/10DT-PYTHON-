37b4816a17d74bd1cb27b0a2b7694fc0fe59f222
fraseroregansmith <103541186+fraseroregansmith@users.noreply.github.com>
2022-08-31 10:31:44 +1200

    Update python_practice_task.py

--- Lesson1/python_practice_task.py
+++ Lesson1/python_practice_task.py
@@ -1,5 +1,5 @@
-# Fraser O'Regan Smith
-# Practice Task
+# Fraser O'Regan Smith ( fictional character)
+# Practice Task - variables, print, data type, comments
 
 #Variable Declaration
 
@@ -9,9 +9,41 @@ height = "179cm"
 adress = "308 Negra Arroyo Lane"
 telephoneNumber = "(505) 193-0809"
 HasAllergy = False
+age = 50
+nationality = "New Zealander!"
+favFood = "Chocolate"
+favFood2 = "Pie"
+# A summary of information is printed out to the screens, clearly
 
 print( "My name is", name)
 print("I am ", height)
 print("I live at",adress)
 print("Phone number",telephoneNumber)
 print("To say that I have an allergy, that would be a", HasAllergy, "statement.")
+print("I am", age, "years old")
+print("I am a proud", nationality,)
+print("My favourite foods are", favFood,"and", favFood2,)
+print("This has been a short bio about me!")
+
+# height is in metres
+# street number and street ( fictional)
+# fictional phone number
+# allergy commands words well
+# age in years
+# nationality (country)
+# favourite foods - tested out having 2 variables in one 
+
+
+
+# This prints out as 
+"""My name is Walter White
+I am  179cm
+I live at 308 Negra Arroyo Lane
+Phone number (505) 193-0809
+To say that I have an allergy, that would be a False statement.
+I am 50 years old
+I am a proud New Zealander!
+My favourite foods are Chocolate and Pie
+This has been a short bio about me!""" 
+
+# Success
