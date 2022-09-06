ccf0cee316c35437911c5c9e3c286971c8ba9dd6
fraseroregansmith <103541186+fraseroregansmith@users.noreply.github.com>
2022-09-07 10:50:46 +1200

    Add files via upload

---
+++ Lesson2/Problem1.py
@@ -0,0 +1,11 @@
+# E M Peterson
+# Calculation of Variables, framing of labels, data in context 
+
+# Problem 01 
+# This program is designed to track and total goals scored by the scots 3rd XI for each game played. 
+
+match1_ST_Pats = 7 
+match2_Well_College = 5
+match3_Well_high = 10
+Team_Total = match1_ST_Pats + match2_Well_College + match3_Well_high
+print ("Total Goals Scored", Team_Total)
